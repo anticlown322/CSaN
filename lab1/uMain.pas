ae397ae7ffc80fc888665e19df9209799559f472
anticlown322 <karasandrey2005@gmail.com>
2024-02-20 20:10:15 +0300

    feat: implement UI

--- lab1/uMain.pas
+++ lab1/uMain.pas
@@ -1,24 +1,115 @@
-unit uMain;
+Unit uMain;
 
-interface
+Interface
 
-uses
-  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+Uses
+    Winapi.Windows,
+    Winapi.Messages,
+    System.SysUtils,
+    System.Variants,
+    System.Classes,
+    Vcl.Graphics,
+    Vcl.Controls,
+    Vcl.Forms,
+    Vcl.Dialogs,
+    Vcl.Buttons,
+    Vcl.StdCtrls,
+    Vcl.VirtualImage,
+    Vcl.ComCtrls,
+    Vcl.ExtCtrls,
+    System.Actions,
+    Vcl.ActnList,
+    Vcl.PlatformDefaultStyleActnCtrls,
+    Vcl.ActnMan,
+    System.ImageList,
+    Vcl.ImgList,
+    Vcl.VirtualImageList;
 
-type
-  TForm1 = class(TForm)
-  private
-    { Private declarations }
-  public
-    { Public declarations }
-  end;
+Type
+    TfrmMain = Class(TForm)
+        PgcSections: TPageControl;
+        TsWelcome: TTabSheet;
+        TsMain: TTabSheet;
+        VimWelcomeBackground: TVirtualImage;
+        PNav: TPanel;
+        SpdbtnTurningGrille: TSpeedButton;
+        SpdbtnVigenere: TSpeedButton;
+        SpdbtnExit: TSpeedButton;
+        SpdbtnHelp: TSpeedButton;
+        VilButtons48: TVirtualImageList;
+        TsHelp: TTabSheet;
+        ActmngActions: TActionManager;
+        ActClose: TAction;
+        ActChooseOption: TAction;
+        ActStartScan: TAction;
+        ActSaveFile: TAction;
+        LbHelp: TLabel;
+        LbHelpTitle: TLabel;
+        LbScanerTitle: TLabel;
+        POutput: TPanel;
+        MmOutput: TMemo;
+        ShpVigenereKey: TShape;
+        SpdbtnSaveFile: TSpeedButton;
+        SpdbtnScan: TSpeedButton;
+        ActGoToGithub: TAction;
+    lbTradeMark: TLabel;
+        Procedure Dragging(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Integer); // custom
+        Procedure ActCloseExecute(Sender: TObject);
+        Procedure FormShow(Sender: TObject);
+        Procedure ActChooseOptionExecute(Sender: TObject);
+        Procedure FormCreate(Sender: TObject);
+        Procedure ActGoToGithubExecute(Sender: TObject);
+    Private
 
-var
-  Form1: TForm1;
+    End;
 
-implementation
+Var
+    FrmMain: TfrmMain;
+
+Implementation
+
+Uses
+    UdtmdImages,
+    UTScanner,
+    UFormEnhances,
+    ShellApi; // for shellexecute
 
 {$R *.dfm}
+{ form methods }
 
-end.
+Procedure TfrmMain.FormCreate(Sender: TObject);
+Begin
+    PgcSections.ActivePage := TsWelcome;
+End;
+
+Procedure TfrmMain.FormShow(Sender: TObject);
+Begin
+    RoundFormCorners(Self, CtRoundedCornerDefault);
+    RoundCornerOf(POutput, CtRoundedCornerDefault);
+End;
+
+Procedure TFrmMain.Dragging(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Integer);
+Begin
+    UFormEnhances.Dragging(Self);
+End;
+
+{ action manager - scaner }
+
+{ action manager - form }
+
+Procedure TfrmMain.ActChooseOptionExecute(Sender: TObject);
+Begin
+    PgcSections.ActivePageIndex := (Sender As TAction).ActionComponent.Tag;
+End;
+
+Procedure TfrmMain.ActCloseExecute(Sender: TObject);
+Begin
+    Close;
+End;
+
+Procedure TfrmMain.ActGoToGithubExecute(Sender: TObject);
+Begin
+    ShellExecute(Handle, 'open', 'https://github.com/anticlown322', Nil, Nil, SW_NORMAL);
+End;
+
+End.
